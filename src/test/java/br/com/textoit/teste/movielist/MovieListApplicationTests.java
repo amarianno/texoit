c17c598978b6b403cb6e0bfc4fd4906d9cd94cb5
Alberto Marianno de Santana Santos <alberto-marianno.santos@serpro.gov.br>
2021-10-25 23:03:54 -0300

    adicionando testes

--- src/test/java/br/com/textoit/teste/movielist/MovieListApplicationTests.java
+++ src/test/java/br/com/textoit/teste/movielist/MovieListApplicationTests.java
@@ -1,13 +1,48 @@
 package br.com.textoit.teste.movielist;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
-@SpringBootTest
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class MovieListApplicationTests {
 
+	@LocalServerPort
+	int porta;
+
+	@BeforeEach
+	public void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = porta;
+	}
+
 	@Test
-	void contextLoads() {
+	public void deve_trazer_filmes_de_acordo_dados_obtidos() {
+
+		given().log().all()
+				.header("Content-Type", "application/json")
+		.when()
+				.get("/filme/intervalos")
+		.then().log().body()
+				.statusCode(HttpStatus.OK.value())
+				.contentType(ContentType.JSON)
+				.body("min[0].producer", equalTo("Bo Derek"))
+				.body("min[0].interval", equalTo(6))
+				.body("min[0].previousWin", equalTo(1984))
+				.body("min[0].followingWin", equalTo(1990))
+				.body("max[0].producer", equalTo("Bo Derek"))
+				.body("max[0].interval", equalTo(6))
+				.body("max[0].previousWin", equalTo(1984))
+				.body("max[0].followingWin", equalTo(1990))
+		;
+
 	}
 
 }
